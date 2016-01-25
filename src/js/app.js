cadbb0bac6e034ed6d4c0398f0d99d89f2955e53
Kaeserkiev <kaeserkiev@gmail.com>
2016-01-25 13:11:09 +0200

    template loaded

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+//ie fix
+if(!(window.console && console.log)) {
+	console = {
+		log: function(){},
+		debug: function(){},
+		info: function(){},
+		warn: function(){},
+		error: function(){}
+	};
+}
+
+$(document).ready(function() {
+
+
+});
